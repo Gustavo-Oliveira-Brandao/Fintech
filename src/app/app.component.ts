8e0bbae346df1e00c0914fb366c06d8fa58f908c
GustavoOB <112260397+Gustavo-Oliveira-Brandao@users.noreply.github.com>
2024-07-23 01:37:20 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SaldoComponent } from './componentes/saldo/saldo.component';
+import { UsuarioService } from './shared/servicos/usuario.service';
+import { HistoricoTransacoesComponent } from './componentes/historico-transacoes/historico-transacoes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SaldoComponent, HistoricoTransacoesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'fintech';
+
+  constructor(protected usuario:UsuarioService){}
 }
---
+++ src/app/componentes/saldo/saldo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saldo',
+  standalone: true,
+  imports: [],
+  templateUrl: './saldo.component.html',
+  styleUrl: './saldo.component.sass'
+})
+export class SaldoComponent {
+
+  saldo:number = 0;
+}
